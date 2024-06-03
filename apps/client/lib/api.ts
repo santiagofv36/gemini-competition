c4c95d6b760e5ca86855bf98a190f17fb09dd22a
santiagofv36 <59099144+santiagofv36@users.noreply.github.com>
2024-06-03 18:16:06 -0400

    fix(client): :zap: fixing google auth & google token gathering

---
+++ apps/client/lib/api.ts
@@ -0,0 +1,9 @@
+import Axios from 'axios';
+
+export const axios = Axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
--- apps/client/lib/auth.ts
+++ apps/client/lib/auth.ts
@@ -2,8 +2,9 @@ import GoogleProvider from 'next-auth/providers/google';
 // import GithubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { NextAuthOptions } from 'next-auth';
-import axios from 'axios';
 import { User } from '@shared/index';
+import { axios } from './api';
+import { AxiosError } from 'axios';
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -30,13 +31,10 @@ export const authOptions: NextAuthOptions = {
           return null;
         }
         const { email, password } = credentials;
-        const res = await axios.post<User>(
-          process.env.NEXT_PUBLIC_API_URL + '/auth/login',
-          {
-            email,
-            password,
-          }
-        );
+        const res = await axios.post<User>('/auth/login', {
+          email,
+          password,
+        });
 
         if (res.status === 401) {
           return null;
@@ -47,13 +45,53 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) token.user = user;
+    async jwt({ token, account }) {
+      if (account?.access_token) {
+        token.access_token = account.access_token;
+      }
       return token;
     },
     async session({ token, session }) {
       if (token.user) session.user = token.user as any;
       return session;
+    },
+    async signIn({ user, account }): Promise<any> {
+      if (account && account.provider === 'google') {
+        const res = await axios.get<User>('/user', {
+          data: {
+            email: user.email,
+          },
+        });
+        /**
+         * @param account{
+          provider: string,
+          type: string,
+          providerAccountId: string,
+          access_token: string,
+          expires_at: number,
+          scope: string,
+          token_type: 'Bearer',
+          id_token: string,
+          }
+         */
+
+        if (Number(res?.data?.status) === 400) {
+          try {
+            const userRes = await axios.post<User>('/auth/signup', {
+              email: user.email,
+              name: user.name,
+              password: 'Gmail-Automator,%998',
+            });
+
+            return userRes.data;
+          } catch (e) {
+            console.log(e);
+            return null;
+          }
+        }
+
+        return res.data;
+      }
     },
   },
   pages: {
